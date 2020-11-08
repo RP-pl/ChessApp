bdd683a7b3126e825abea096b9c5804bb3c15702
RP-pl <68811106+RP-pl@users.noreply.github.com>
2020-11-08 17:48:13 +0100

    Add files via upload

--- ChessAppAI/Model.py
+++ ChessAppAI/Model.py
@@ -2,7 +2,7 @@ import numpy
 import tensorflow.keras as keras
 
 from ChessAppAI import DSManip
-from ChessAppAI.DSManip import prerpareDatasets,getData
+from ChessAppAI.DSManip import getData
 from ChessAppAI.Tokenizer import Token
 
 model = keras.models.Sequential([
@@ -14,15 +14,17 @@ model = keras.models.Sequential([
     keras.layers.Dropout(0.25),
     keras.layers.GRU(128,activation='elu',return_sequences=False),
     keras.layers.Flatten(),
-    keras.layers.Dense(6488,activation='elu'),
+    keras.layers.Dense(1024,activation='selu'),
+    keras.layers.Dense(6488,activation='elu')
 ])
 model = keras.models.load_model('ChessAI.h5')
-model.compile(optimizer=keras.optimizers.Adadelta(learning_rate=0.015),loss=keras.losses.categorical_hinge,metrics=[keras.metrics.LogCoshError(),keras.metrics.Accuracy()])
+model.compile(optimizer=keras.optimizers.Adadelta(learning_rate=0.002),loss=keras.losses.categorical_hinge,metrics=[keras.metrics.LogCoshError(),keras.metrics.Accuracy(),keras.metrics.binary_crossentropy])
 t = Token()
-i=9
+i=6
 x,y = getData(t,i*4000,(i+1)*4000)
-model.fit(x,y,epochs=10,callbacks=[keras.callbacks.ModelCheckpoint(filepath="ChessAI.h5",save_weights_only=False,save_best_only=True,monitor='loss'),keras.callbacks.EarlyStopping(monitor='loss'),keras.callbacks.TensorBoard('logs/tendorboard')])
-test = DSManip.prepareFit(t,"W1.e4 B1.e5 W2.Nf3")
+model.summary()
+model.fit(x,y,epochs=20,callbacks=[keras.callbacks.ModelCheckpoint(filepath="ChessAI.h5",save_weights_only=False,save_best_only=True,monitor='loss'),keras.callbacks.EarlyStopping(monitor='loss'),keras.callbacks.TensorBoard('logs/tendorboard')])
+test = DSManip.prepareFit(t,"W1.e4 B1.e5 W2.Nc3")
 print(model.predict(test))
 print(numpy.argmax(model.predict(test)))
 print(t.t.index_word[numpy.argmax(model.predict(test))])
